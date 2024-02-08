49a1fc6a1f25e1e15a51ea8eeac991ce124fd4c7
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-08 16:03:35 -0300

    feat: ThemeToggle and account menu

--- src/app.tsx
+++ src/app.tsx
@@ -5,16 +5,19 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Toaster } from '@/components/ui/sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <Toaster richColors />
+        <Toaster richColors />
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
