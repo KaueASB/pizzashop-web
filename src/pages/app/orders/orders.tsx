ca8c9f419b6f685d1a4435cac3e810de2885a24b
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-08 18:52:23 -0300

    feat: API connection - Order filters

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -15,35 +15,48 @@ import {
 
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  const orderId = searchParams.get('orderId') ?? ''
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
 
   const pageIndex = z.coerce
     .number()
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
-    queryKey: ['orders', pageIndex],
-    queryFn: () => getOrders({ pageIndex }),
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
+    queryKey: ['orders', pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        orderId,
+        customerName,
+        status: status === 'all' ? null : status,
+      }),
   })
 
   function handlePaginate(pageIndex: number) {
-    setSearchParams((state) => {
-      state.set('page', (pageIndex + 1).toString())
+    setSearchParams((prevStateParams) => {
+      prevStateParams.set('page', (pageIndex + 1).toString())
 
-      return state
+      return prevStateParams
     })
   }
 
   return (
     <>
       <Helmet title="Pedidos" />
+
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
           <OrderTableFilters />
+
           <div className="rounded-md border">
             <Table>
               <TableHeader>
@@ -59,6 +72,7 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
                 {result &&
                   result.orders.map((order) => {
                     return <OrderTableRow key={order.orderId} order={order} />
@@ -69,10 +83,10 @@ export function Orders() {
 
           {result && (
             <Pagination
+              pageIndex={result?.meta.pageIndex}
+              perPage={result?.meta.perPage}
+              totalCount={result?.meta.totalCount}
               onPageChange={handlePaginate}
-              pageIndex={result.meta.pageIndex}
-              totalCount={result.meta.totalCount}
-              perPage={result.meta.perPage}
             />
           )}
         </div>
