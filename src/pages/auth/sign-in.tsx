67f544b49a2547efa24d8e7a44d1c15ccdc02dbc
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-08 15:45:14 -0300

    feat: register restaurants

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -16,11 +17,9 @@ export function SignIn() {
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<SignInForm>()
-
   async function handleSignIn(data: SignInForm) {
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000))
-
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
           label: 'Reenviar',
@@ -33,11 +32,15 @@ export function SignIn() {
       toast.error('Credenciais inválidas.')
     }
   }
-
   return (
     <>
       <Helmet title="Login" />
+
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
