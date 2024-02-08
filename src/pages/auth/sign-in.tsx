f1fe73bc32f50bc72cc36dbd3ec853664651add4
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-08 15:34:07 -0300

    feat: toast notification

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,18 +1,15 @@
-
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-
 const signInForm = z.object({
   email: z.string().email(),
 })
-
 type SignInForm = z.infer<typeof signInForm>
-
 export function SignIn() {
   const {
     register,
@@ -21,9 +18,20 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          },
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
@@ -39,13 +47,11 @@ export function SignIn() {
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
-
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" {...register('email')} />
             </div>
-
             <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
