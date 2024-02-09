85d24f2c618c7fa4706c3640e79c945ecc355926
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-09 11:02:36 -0300

    test: Component - Order status

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -21,19 +21,28 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {['pending'].includes(status) && (
-        <span className="size-2 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="size-2 rounded-full bg-slate-400"
+        />
       )}
 
       {['canceled'].includes(status) && (
-        <span className="size-2 rounded-full bg-rose-500" />
+        <span data-testid="badge" className="size-2 rounded-full bg-rose-500" />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="size-2 rounded-full bg-amber-500" />
+        <span
+          data-testid="badge"
+          className="size-2 rounded-full bg-amber-500"
+        />
       )}
 
       {['delivered'].includes(status) && (
-        <span className="size-2 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="size-2 rounded-full bg-emerald-500"
+        />
       )}
 
       <span className='font-medium" text-muted-foreground'>
